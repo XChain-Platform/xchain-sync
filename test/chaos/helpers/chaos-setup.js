XChain-Platform/xchain-explorer

test: parameterize fixture host ports
Derive published fixture ports from the gate CI_PORT_OFFSET while preserving zero-offset defaults. Keep container ports fixed and route fixture clients through the same resolved host ports.

test/chaos/helpers/chaos-setup.js
@@ -36,8 +36,9 @@ const toxiproxy      = require('./toxiproxy-client');
 const mariadb        = require('mariadb');
 const fs             = require('fs');
 const path           = require('path');
+const fixturePorts   = require('../../../bin/fixture-ports.js');
 
-const DB_PORT = 3307;  // toxiproxy's proxied port
+const DB_PORT = fixturePorts.port('XCHAIN_EXPLORER_CHAOS_DB_PORT');
 
 let server      = null;
 let serverUrl   = null;

XChain-Platform/xchain-sync

test/chaos/helpers/chaos-setup.js
@@ -32,12 +32,13 @@ const testDb        = require('../../e2e/helpers/testDb');
 const fixtures      = require('../../e2e/helpers/fixtures');
 const ServerProcess = require('../../e2e/helpers/serverProcess');
 const ClientProcess = require('../../e2e/helpers/clientProcess');
+const fixturePorts = require('../../../bin/fixture-ports.js');
 
 // Connection constants: proxied ports from docker-compose.chaos.yml
 const CHAOS_DB_HOST      = process.env.CHAOS_DB_HOST || '127.0.0.1';
-const SOURCE_PROXY_PORT  = parseInt(process.env.SOURCE_PROXY_PORT  || '33060', 10);
-const REPLICA_PROXY_PORT = parseInt(process.env.REPLICA_PROXY_PORT || '33061', 10);
-const SOURCE_DIRECT_PORT = parseInt(process.env.SOURCE_DIRECT_PORT || '33065', 10);
+const SOURCE_PROXY_PORT  = fixturePorts.port('SOURCE_PROXY_PORT');
+const REPLICA_PROXY_PORT = fixturePorts.port('REPLICA_PROXY_PORT');
+const SOURCE_DIRECT_PORT = fixturePorts.port('SOURCE_DIRECT_PORT');
 const CHAOS_DB_USER      = 'xchain-node';
 const CHAOS_DB_PASS      = 'xchain-fixture-throwaway';
 